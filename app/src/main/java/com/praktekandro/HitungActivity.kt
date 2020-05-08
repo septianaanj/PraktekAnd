e468084c03c279b67d7295b0a2f8a74ac44e893d
septianaanj <septyanadila@gmail.com>
2020-05-08 16:25:39 +0800

    Menghitung Volume dan Validasinya

--- app/src/main/java/com/praktekandro/HitungActivity.kt
+++ app/src/main/java/com/praktekandro/HitungActivity.kt
@@ -2,11 +2,44 @@ package com.praktekandro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_hitung.*
 
 class HitungActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hitung)
+
+        buttonHitung.setOnClickListener {
+            var isEmptyField = false
+
+            val stringPanjang = editPanjang.text.toString().trim()
+            val stringLebar = editLebar.text.toString().trim()
+            val stringTinggi = editTinggi.text.toString().trim()
+
+            if(stringPanjang.equals("")){
+                isEmptyField = true
+                editPanjang.error = "Data kosong"
+            }
+
+            if(stringLebar.equals("")){
+                isEmptyField = true
+                editLebar.error = "Data kosong"
+            }
+
+            if(stringTinggi.equals("")){
+                isEmptyField = true
+                editTinggi.error = "Data kosong"
+            }
+
+            if(!isEmptyField) {
+
+                val panjang = stringPanjang.toFloat()
+                val lebar = stringLebar.toFloat()
+                val tinggi = stringTinggi.toFloat()
+
+                textHasil.text = (panjang * lebar * tinggi).toString()
+            }
+        }
     }
 }
